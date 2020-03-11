cf989d79ffc03fbad0eacec5073736ef157b6bc7
Andrey Osintsev <ozzy.ext@gmail.com>
2020-03-12 00:18:23 +0300

    Fix publish script

--- ci/publish.bat
+++ ci/publish.bat
@@ -3,7 +3,7 @@ echo off
 IF [%1]==[] goto noparam
 
 echo "Build project ..."
-dotnet publish ..\src\MyAuth.OAuthPoint\MyAuth.OAuthPoint.proj -r Release -o .\out\app
+dotnet publish ..\src\MyAuth.OAuthPoint\MyAuth.OAuthPoint.csproj -c Release -o .\out\app
 
 echo "Build image '%1' and 'latest'..."
 docker build -t ozzyext/myauth-oauth-point:%1 -t ozzyext/myauth-oauth-point:latest .
